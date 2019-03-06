2898f10be758decdc85ba6c523cb5be6b9092855
Dejan Bosanac <dejan@nighttale.net>
2019-03-06 16:56:22 +0100

    Better handle the case of parsing malformed MQTT frames

--- mqtt-client/src/main/java/org/fusesource/mqtt/codec/MessageSupport.java
+++ mqtt-client/src/main/java/org/fusesource/mqtt/codec/MessageSupport.java
@@ -62,6 +62,9 @@ public final class MessageSupport {
 
     static protected UTF8Buffer readUTF(DataByteArrayInputStream is) throws ProtocolException {
         int size = is.readUnsignedShort();
+        if (size < 0) {
+            throw new ProtocolException("Invalid message encoding");
+        }
         Buffer buffer = is.readBuffer(size);
         if (buffer == null || buffer.length != size) {
             throw new ProtocolException("Invalid message encoding");
